36e56ecd9e7053a4e1c8eceed1a2faeae5e182a2
Mathias_P <pizzapeter@users.noreply.github.com>
2017-11-10 09:57:13 +0100

    added wpf application
    added sha-256 hash in javaFX
    started implementing wpf application

--- JavaFX_Application/UserManagement/src/gui/LoginWindowController.java
+++ JavaFX_Application/UserManagement/src/gui/LoginWindowController.java
@@ -2,22 +2,55 @@ package gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class LoginWindowController {
     @FXML
     private Label lblStatus;
-
     @FXML
     private PasswordField passwordField;
 
+    private byte[] passwd; //only for testing
+
     public void OnLogin(ActionEvent actionEvent) {
-
-
-        lblStatus.setTextFill(Color.RED);
-        lblStatus.setText("Wrong Password");
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            passwd = digest.digest("password".getBytes(StandardCharsets.UTF_8));
+            byte[] hash = digest.digest(passwordField.getText().getBytes(StandardCharsets.UTF_8));
+            passwordField.clear();
+            if (Arrays.equals(passwd, hash)) {
+                System.out.println("hashing worked");
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+            Stage mainWindow = new Stage();
+            mainWindow.setScene(new Scene(root));
+            mainWindow.setTitle("User Management");
+            mainWindow.getIcons().add(
+                    new Image(Main.class.getResourceAsStream("ic_person_black_24dp_2x.png"))
+            );
+            Stage thisWindow = (Stage) lblStatus.getScene().getWindow();
+            thisWindow.close();
+            mainWindow.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
