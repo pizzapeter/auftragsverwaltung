678e97b7d2a20ecf7ec8bb903ceeeb2cefb8e316
pichlerd1 <dominik.pichler7@gmail.com>
2018-03-16 10:26:39 +0100

    Fix

--- WebService/RESTBSD/src/at/BSD/TaskService.java
+++ WebService/RESTBSD/src/at/BSD/TaskService.java
@@ -27,8 +27,7 @@ public class TaskService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getTasks() {
 		at.Data.Database db = new Database();
-		String selectTableSQL = "SELECT * from task t inner join EmployeeTask et on t.id = et.TaskID inner "
-				+ "join Employees e on et.EmployeeID=e.ID";
+		String selectTableSQL = "SELECT t.ID,t.NAME,t.Description from task t inner join EmployeeTask et on t.id = et.TaskID inner join Employees e on et.EmployeeID=e.ID";
 		JSONArray jarray = new JSONArray();
 		JSONObject jobject = null;
 		ResultSet rs;
@@ -42,10 +41,6 @@ public class TaskService {
 				jobject.put("ID", rs.getObject("t.ID"));
 				jobject.put("Name", rs.getObject("t.NAME"));
 				jobject.put("DESCRIPTION", rs.getObject("t.DESCRIPTION"));
-				jobject.put("FINISHED", rs.getObject("t.FINISHED"));
-				jobject.put("Firstname", rs.getObject("e.FIRSTNAME").toString());
-				jobject.put("Lastname", rs.getObject("e.LASTNAME").toString());
-				jobject.put("PLACEID", rs.getObject("PLACEID"));
 				jarray.add(jobject);
 
 			}
@@ -65,8 +60,12 @@ public class TaskService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getTaksOfEmployee(@PathParam("employeeID") String employeeID) {
 		at.Data.Database db = new Database();
-		String selectTableSQL = "SELECT t.ID from task t inner join EmployeeTask et on t.id = et.TaskID inner "
-				+ "join Employees e on et.EmployeeID=e.ID and e.ID = "+employeeID;
+		StringBuilder st = new StringBuilder();
+		st.append("SELECT t.ID,t.NAME,t.Description from task t inner join ");
+		st.append("EmployeeTask et on t.id = et.TaskID inner join Employees e on ");
+		st.append("et.EmployeeID=e.ID where t.Finished = 0 and et.EMPLOYEEID = "+employeeID);
+		String selectTableSQL = st.toString();
+				
 		JSONArray jarray = new JSONArray();
 		JSONObject jobject = null;
 		ResultSet rs;
@@ -77,13 +76,9 @@ public class TaskService {
 			while (rs.next()) {
 
 				jobject = new JSONObject();
-				jobject.put("ID", rs.getObject("t.ID"));
-				jobject.put("Name", rs.getObject("t.NAME"));
-				jobject.put("DESCRIPTION", rs.getObject("t.DESCRIPTION"));
-				jobject.put("FINISHED", rs.getObject("t.FINISHED"));
-				jobject.put("Firstname", rs.getObject("e.FIRSTNAME").toString());
-				jobject.put("Lastname", rs.getObject("e.LASTNAME").toString());
-				jobject.put("PLACEID", rs.getObject("PLACEID"));
+				jobject.put("ID", rs.getObject("ID"));
+				jobject.put("NAME", rs.getObject("NAME"));
+				jobject.put("Description", rs.getObject("Description"));
 				jarray.add(jobject);
 
 			}
@@ -152,6 +147,23 @@ public class TaskService {
 			return e.getMessage();
 		}
 		return "Inserted";
+	}
+	
+	@GET
+	@Path("/finishtask/{TaskID}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String finishTask(@PathParam("TaskID") String taskID) {
+		at.Data.Database db = new Database();
+		String updateCommand = "UPDATE task SET finished = 1 WHERE ID = " + taskID;
+		try {
+			Statement statment = db.getCon().createStatement();
+			statment.execute(updateCommand);
+			return "updatet";
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return e.getLocalizedMessage();
+		}
 	}
 	
 }
